bcc4ed601b1bed2b1f61c63f383a05286e9b03be
raphaelagil <raphaela.gil@yahoo.com>
2025-05-20 13:04:46 -0300

    updated the tailwind version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}", // aponte para suas pastas de código
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
