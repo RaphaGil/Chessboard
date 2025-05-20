5a0c14610f9cd3aa4cbbdb4a29e951d0c20be4d2
raphaelagil <raphaela.gil@yahoo.com>
2025-05-20 09:18:34 -0300

    chessboard created

---
+++ my-app/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chessboard from './Chessboard'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Chessboard />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Chessboard.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const Chessboard = () => {
+  const [selected, setSelected] = useState<string | null>(null);
+
+  const board = [];
+
+  const files = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+  for (let row = 0; row < 8; row++) {
+    for (let col = 0; col < 8; col++) {
+      const coord = `${files[col]}${row}`;
+      const Dark = (row + col) % 2 === 1;
+      const isSelected = selected === coord;
+
+      board.push(
+        <div
+          key={coord}
+          onClick={() => {
+            setSelected(coord);
+            console.log(coord);
+          }}
+          className={`w-10 h-10 ${
+            Dark ? "bg-yellow-500 text-black" : "bg-gray-800 text-white"
+          }
+       ${isSelected ? "border-4 border-white " : ""}`}
+        >
+          <div>{coord}</div>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-center mb-10 mt-10">CHESSBOARD GAME</h1>
+
+      <div className="grid grid-cols-8 grid-row-8 w-80 border-2 border-black ">
+        {board}
+      </div>
+    </div>
+  );
+};
+
+export default Chessboard;
